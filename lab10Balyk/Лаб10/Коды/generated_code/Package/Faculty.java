6d8794253da5b14ba24ce99662d139e0403c7745
ivan01239 <115571433+ivan01239@users.noreply.github.com>
2024-01-10 15:38:12 +0300

    Add files via upload
    ыыыы

---
+++ lab10Balyk/Лаб10/Коды/generated_code/Package/Faculty.java
@@ -0,0 +1,24 @@
+package Package;
+
+public class Faculty {
+
+	public Employee dean = null;
+	public String name = "";
+	public Date dateOfCreation = null;
+	public String officeAdress = "";
+
+	public double age() {
+		// TODO - implement Faculty.age
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param newOfficeAdress
+	 */
+	public void changeOfficeAdress(String newOfficeAdress) {
+		// TODO - implement Faculty.changeOfficeAdress
+		throw new UnsupportedOperationException();
+	}
+
+}
